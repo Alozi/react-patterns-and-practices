ddaed7293713f41e6f5e243ea58fe62297bcdab4
Alozi <alozimozi@gmail.com>
2025-07-17 17:33:38 +0300

    Create components Accordion and AccordionItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <h1>React Patterns & Practices</h1>
+      <section>
+        <h2>Why you should work with us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem
+            title="We got 20 years of experience"
+            className="accordion-item"
+          >
+            <article>
+              <p>You can't go wrong with us.</p>
+              <p>
+                We are in the business of planning highly individualized
+                vacation trips for more than 20 years.
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem
+            title="We'are working with local guides."
+            className="accordion-item"
+          >
+            <article>
+              <p>We are not doing this along from our office.</p>
+              <p>
+                Instead, we are working with local guides to ensure a safe and
+                pleasant vacation.
+              </p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
