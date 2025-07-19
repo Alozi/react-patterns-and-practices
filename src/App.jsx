4592c839e9d7d07dc8efe670636812a8af2a65a6
Alozi <alozimozi@gmail.com>
2025-07-19 14:34:27 +0300

    Adding extra components for reusability

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Accordion from "./components/Accordion/Accordion";
-import AccordionItem from "./components/Accordion/AccordionItem";
 
 function App() {
   return (
@@ -8,31 +7,39 @@ function App() {
       <section>
         <h2>Why you should work with us?</h2>
         <Accordion className="accordion">
-          <Accordion.Item
-            id="experience"
-            title="We got 20 years of experience"
-            className="accordion-item"
-          >
-            <article>
-              <p>You can't go wrong with us.</p>
-              <p>
-                We are in the business of planning highly individualized
-                vacation trips for more than 20 years.
-              </p>
-            </article>
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title id="experience" className="accordion-item-title">
+              We got 20 years of experience
+            </Accordion.Title>
+            <Accordion.Content
+              id="experience"
+              className="accordion-item-content"
+            >
+              <article>
+                <p>You can't go wrong with us.</p>
+                <p>
+                  We are in the business of planning highly individualized
+                  vacation trips for more than 20 years.
+                </p>
+              </article>
+            </Accordion.Content>
           </Accordion.Item>
-          <Accordion.Item
-            id="local-guides"
-            title="We'are working with local guides."
-            className="accordion-item"
-          >
-            <article>
-              <p>We are not doing this along from our office.</p>
-              <p>
-                Instead, we are working with local guides to ensure a safe and
-                pleasant vacation.
-              </p>
-            </article>
+          <Accordion.Item id="local-guides" className="accordion-item">
+            <Accordion.Title id="local-guides" className="accordion-item-title">
+              We'are working with local guides.
+            </Accordion.Title>
+            <Accordion.Content
+              id="local-guides"
+              className="accordion-item-content"
+            >
+              <article>
+                <p>We are not doing this along from our office.</p>
+                <p>
+                  Instead, we are working with local guides to ensure a safe and
+                  pleasant vacation.
+                </p>
+              </article>
+            </Accordion.Content>
           </Accordion.Item>
         </Accordion>
       </section>
---
+++ src/components/Accordion/AccordionTitle.jsx
@@ -0,0 +1,16 @@
+import { useAccordionContext } from "./Accordion";
+
+export default function AccordionTitle({ id, children, className }) {
+  const { toogleItem } = useAccordionContext();
+
+  return (
+    <h3
+      className={className}
+      onClick={() => {
+        toogleItem(id);
+      }}
+    >
+      {children}
+    </h3>
+  );
+}
